940a8f70b4d6e36bafcee4d2a7f4b4664a95566a
Manorkie25 <manorkie96@gmail.com>
2025-05-30 12:06:09 -0700

    Work Ready

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.getElementById('nav-toggle');
+  const navMenu = document.getElementById('nav-menu');
+  if (navToggle && navMenu) {
+    navToggle.addEventListener('click', () => {
+      navMenu.classList.toggle('hidden');
+    });
+  }
+});
